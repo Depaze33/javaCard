80108c28443a08e595116b4d3d4b0a8d7ad11f4d
quentinmathieu <quentin.mth@gmail.com>
2024-07-31 16:31:02 +0200

    grid contacts added for the main frame

--- src/main/java/fr/afpa/App.java
+++ src/main/java/fr/afpa/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("listContacts"), 640, 480);
+        scene = new Scene(loadFXML("listContacts"));
         stage.setScene(scene);
         stage.show();
     }
