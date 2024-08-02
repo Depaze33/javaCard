c7f9a7a0fdaa85d6e53c107ec67b797a99978100
Clarisse Otmani <clarisseotm@gmail.com>
2024-08-02 11:32:25 +0200

    continue creationContactController

--- src/main/java/fr/afpa/CreationContactController.java
+++ src/main/java/fr/afpa/CreationContactController.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
@@ -26,6 +28,44 @@ private GridPane buttonGridPane;
 
     @FXML
     private Label requiredLabel;
+
+@FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    private TextField firstNameTextField;
+
+    @FXML
+    private MenuButton genderMenuButton;
+
+    @FXML
+    private TextField birthDayTextField;
+
+    @FXML
+    private TextField pseudoTextField;
+
+    @FXML
+    private TextField adressTextField;
+    @FXML
+    private TextField personalTextField;
+
+    @FXML
+    private TextField proffesionnalTextField;
+
+    @FXML
+    private TextField mailAdressTextField;
+
+    @FXML
+    private TextField postalAdressTextField;
+
+    @FXML
+    private TextField gitTextField;
+
+
+
+   
+
+
 
     public void initialize() throws ClassNotFoundException, IOException {
 
