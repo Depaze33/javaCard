f6d6455e21c54175c1cd554cd704ea3d1ba4ccbb
Clarisse Otmani <clarisseotm@gmail.com>
2024-08-05 16:34:26 +0200

    Modifciaiton class CreationContactController

--- src/main/java/fr/afpa/CreationContactController.java
+++ src/main/java/fr/afpa/CreationContactController.java
@@ -5,8 +5,13 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
@@ -32,6 +37,28 @@ public class CreationContactController {
     private GridPane buttonGridPane;
 
     @FXML
+    private Label lastNameLabel;
+
+    @FXML
+    private Label firstNameLabel;
+
+    @FXML
+    private Label genderLabel;
+
+    @FXML
+    private Label birthDayLabel;
+
+  
+    @FXML
+    private Label personalPhoneLabel;
+
+    @FXML
+    private Label mailLabel;
+
+    @FXML
+    private Label postalAdressLabel;
+    
+    @FXML
     private Button buttonExportJson;
 
     @FXML
@@ -56,15 +83,13 @@ public class CreationContactController {
     private TextField firstNameTextField;
 
     @FXML
-    private MenuButton genderMenuButton;
-
-    @FXML
     private TextField birthDayTextField;
+    @FXML
+    private ComboBox<String> comboBoxGender;
 
     @FXML
     private TextField pseudoTextField;
 
-    
     @FXML
     private TextField personalNumberTextField;
 
@@ -83,13 +108,40 @@ public class CreationContactController {
     @FXML
     private Button saveButton;
 
+    @FXML
+    private VBox vBoxErrors;
+
+    public void initialize() {
+
+        // ONLY DEBUG CODE
+        birthDayTextField.setText("2021-01-01");
+
+        ////
+
+        // hide the edit button
+        buttonEdit.setVisible(false);
+        buttonDelete.setVisible(false);
+
+        // add elemetns to gender ComboBox
+        ObservableList<String> genders = FXCollections.observableArrayList();
+        genders.add("M");
+        genders.add("F");
+        genders.add("NB");
+        comboBoxGender.setItems(genders);
+        comboBoxGender.getSelectionModel().selectFirst();
+    }
+
     // Methode save contact
-    public boolean persistContact() throws ClassNotFoundException, IOException {
+    public void persistContact() throws ClassNotFoundException, IOException {
+        // get all the information from the fields
+        // declaration of variables for each information
+
+        StringBuilder errorMessages = new StringBuilder();
         String lastName = lastNameTextField.getText();
+
         String firstName = firstNameTextField.getText();
-        String gender = genderMenuButton.getText();
-        LocalDate birthDate = LocalDate.parse(birthDayTextField.getText(), 
-        DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String gender = comboBoxGender.getPromptText();
+        LocalDate birthDate = LocalDate.now();
         String pseudo = pseudoTextField.getText();
         String personnalNumber = personalNumberTextField.getText();
         String professionalNumber = professionalTextField.getText();
@@ -97,32 +149,67 @@ public class CreationContactController {
         String postalAddress = postalAddressTextField.getText();
         String git = gitTextField.getText();
 
-        Contact newContact = new Contact(
-                lastName,
-                firstName,
-                gender,
-                birthDate,
-                pseudo,
-                personnalNumber,
-                professionalNumber,
-                mailAddress,
-                postalAddress,
-                git);
+        // Reset error message if validation passes
 
-        // Load existing contacts
-        ArrayList<Contact> contacts = App.deserializerMethod();
+        boolean error = false;
+        // regex and required fields
+        if (lastName.trim().isEmpty()) {
+            lastNameLabel.getStyleClass().add("labelRequired");
+            error = true;
+        }
 
-        // Add the new contact to the list
-        contacts.add(newContact);
+        if (firstName.trim().isEmpty()) {
+            firstNameLabel.getStyleClass().add("labelRequired");
+            error = true;
+        }
 
-        System.out.println(contacts);
 
-        // Save the updated list of contacts
-        App.serializerMethode(contacts);
+        if (birthDate == null) {
+            // La date est null
+            birthDayLabel.getStyleClass().add("labelRequired");
+        }
+       
+        if (personnalNumber.trim().isEmpty()) {
+            personalPhoneLabel.getStyleClass().add("labelRequired");
+            error = true;
+        }
+        if (mailAddress.trim().isEmpty()) {
+            mailLabel.getStyleClass().add("labelRequired");
+            error = true;
+        }
 
-        System.out.println("Contact saved: " + newContact);
+        if (postalAddress.trim().isEmpty()) {
+            postalAdressLabel.getStyleClass().add("labelRequired");
+            error = true;
+        }
 
-        return false;
+    
 
+        if (!error) {
+            // instanciation of a new Contact object
+            Contact newContact = new Contact(
+                    lastName,
+                    firstName,
+                    gender,
+                    birthDate,
+                    pseudo,
+                    personnalNumber,
+                    professionalNumber,
+                    mailAddress,
+                    postalAddress,
+                    git);
+
+            // Load existing contacts
+            ArrayList<Contact> contacts = App.deserializerMethod();
+            // Add the new contact to the list
+            contacts.add(newContact);
+
+            System.out.println(contacts);
+
+            // Save the updated list of contacts
+            App.serializerMethode(contacts);
+            App.setRoot("contactList");
+            System.out.println("Contact saved: " + newContact);
+        }
     }
 }
